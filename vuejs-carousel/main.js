5514706fbebefeda8cb1f310b4ed15ba47dffdf0
MGF2 <fioremartina@hotmail.com>
2020-11-02 18:00:04 +0100

    added nav dots and autoplay

--- vuejs-carousel/main.js
+++ vuejs-carousel/main.js
@@ -2,11 +2,32 @@ var app = new Vue ({
   el: '#app',
   data: {
     counter: 0,
+    auto: null,
+    // images: [
+    //   'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+    //   'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+    //   'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+    //   'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+    // ]
+
+    //CON ARRAY DI OGGETTI
     images: [
-      'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
-      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
-      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
-      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+      {
+        src: 'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+        alt: 'landscape1'
+      },
+      {
+        src: 'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+        alt: 'landscape2'
+      },
+      {
+        src: 'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+        alt: 'landscape3'
+      },
+      {
+        src: 'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg',
+        alt: 'landscape4'
+      }
     ]
   },
   methods: {
@@ -23,6 +44,14 @@ var app = new Vue ({
       } else {
         this.counter--;
       }
+    },
+    change(index) {
+      clearInterval(this.auto);
+      this.counter = index;
     }
+  },
+  mounted:function() {
+    //autoplay
+    this.auto = setInterval(this.next,2000); //metodo senza parentesi
   }
 });
